656e9d11e2fad5cd0b5d253787c3ff7024e357da
Momomash <momomash110697@gmail.com>
2021-08-03 22:39:08 +0300

    add depth arg

--- homework-1/getDirectoryTree/index.js
+++ homework-1/getDirectoryTree/index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-function getDirectoryTree(dir) {
+function getDirectoryTree(dir, depth = -1) {
     const structure = {name: dir.split('/').pop(), items: []}
     const files = fs.readdirSync(dir);
     files.forEach(name => {
         const absolutePath = `${dir}/${name}`
         let newItem = {name}
-        if (fs.statSync(absolutePath).isDirectory()) {
-            newItem = getDirectoryTree(absolutePath)
+        if (fs.statSync(absolutePath).isDirectory() && depth !== 0) {
+            newItem = getDirectoryTree(absolutePath, depth - 1)
         }
         structure.items.push(newItem)
     })
