3dcf625958d31f19b455f1849c25b7d0853369ba
Momomash <momomash110697@gmail.com>
2021-08-20 18:51:56 +0300

    add step 0

---
+++ homework-2/commonConstants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MEGABYTE: 1024 * 1024,
+    INITIAL_FILE_PATH: "initialFile.txt"
+}
---
+++ homework-2/script.js
@@ -0,0 +1,7 @@
+const createInitialFileWithRandomNumbers = require('./steps/step1')
+const commonConstants = require('./commonConstants');
+
+// Step 0
+createInitialFileWithRandomNumbers(commonConstants.MEGABYTE * 100)
+
+
